a73f0682a247b5cb4b684123166da21183d9de93
Aspect <amirandshamar@gmail.com>
2020-08-29 10:34:16 -0700

    Create Challenge1.py

---
+++ Challenge1.py
@@ -0,0 +1,7 @@
+//Based on the name of the person, return their relationship to luke
+def relation_to_luke(name):
+	solutions = {'Leia':'sister',
+	'Darth Vader':'father',
+	'Han':'brother in law',
+	'R2D2':'droid'}
+	return 'Luke, I am your {}.'.format(solutions[name])
