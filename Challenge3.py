652a5c4f6fb3a3d4d88e425fbe86e6264df9541a
Aspect <amirandshamar@gmail.com>
2020-08-31 12:34:55 -0700

    Update Challenge3.py

--- Challenge3.py
+++ Challenge3.py
@@ -6,5 +6,5 @@ def stutter(word):
  return word[:2] + "... " + word[:2] + "... " + word + "?"
 
 #most upvoted solution 
-def stutter(word):
+def stutter2(word):
 		return '{0}... {0}... {1}?'.format(word[:2], word)
